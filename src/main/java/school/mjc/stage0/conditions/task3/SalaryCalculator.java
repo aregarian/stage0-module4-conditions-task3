a164d1c29118aeb0fa56ced1a48d1ce8c160c1c8
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-19 01:24:49 +0300

    ascgwgfFWAF

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        float salar = salary;
+        if (salary < 0) {
+            System.out.println("wrong input");
+        }
+        if (salary > 0 && salary <= 10000) {
+            System.out.println(salar * 15 / 100);
+        }
+        if (salary > 10000 && salary <= 20000) {
+            System.out.println(salar * 18 / 100);
+        }
+        if (salary > 20000) {
+            System.out.println(salar * 20 / 100);
+        }
+
     }
 }
