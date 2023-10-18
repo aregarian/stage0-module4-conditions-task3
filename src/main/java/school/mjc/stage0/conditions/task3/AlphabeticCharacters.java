946f55b158f792c92612dc645a0e6cc46538a811
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-19 02:35:17 +0300

    sdgseg erseseeassafgdge

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,10 +3,10 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
 
-      if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'B' ) {
-            System.out.println("Vowel");
-        }else if (character < 'a' || character > 'z') {
+        if (!(character >= 'a' && character <= 'z') && !(character >= 'A' && character <= 'Z')) {
             System.out.println("wrong alphabet!");
+        }else if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ) {
+            System.out.println("Vowel");
         } else {
             System.out.println("Consonant");
         }
